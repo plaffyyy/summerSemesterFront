ee2a291c222799d47cce8d5b99639fcf4db6ee56
plaffyyy <k.zimin@innopolis.university>
2024-07-03 13:31:12 +0300

    lab 4 version

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/summerSemesterFront/',
-  plugins: [react()]
+  plugins: [react()],
+  viteStaticCopy({
+    targets: [
+      { src: 'public/img', dest: 'img' }
+    ]
+  })
 })
