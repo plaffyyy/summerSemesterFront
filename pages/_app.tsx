5d3f6ac0208639cf6342efb7d87f79396718ad3c
plaffyyy <k.zimin@innopolis.university>
2024-07-06 09:31:33 +0300

    home 5 version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 // pages/_app.tsx
 import { AppProps } from 'next/app';
 import React from 'react';
-import '/summerSemesterFront/styles/forInitPage.css';
-import '/summerSemesterFront/styles/lab2.css'
-import '/summerSemesterFront/styles/abMe.css'
-import '/summerSemesterFront/styles/portfol.css'
+import '../styles/forInitPage.css';
+import '../styles/lab2.css'
+import '../styles/abMe.css'
+import '../styles/portfol.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
